9b9f10ae3c466d576eed226d7ab2a71138ee955f
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-07-09 15:29:17 +0300

    updates on the test file for the deleteTodo func Relates #4

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+var state = [
+  { id: -3, description: 'first todo' },
+  { id: -2, description: 'second todo' },
+  { id: -1, description: 'third todo' },
+];
+
+test('Testing the delete function', function(t) {
+  let actual=(logic.deleteTodo(state,-2));
+  let expected=([
+    { id: -3, description: 'first todo' },
+    { id: -1, description: 'third todo' },
+  ])
+  t.deepEqual(actual,expected,"The new Array must not includes the id -2");
   t.end();
 });
