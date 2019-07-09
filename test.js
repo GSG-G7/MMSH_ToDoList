6ef132f1cd1741524540e5029fcf50d0ea2868ad
MaiUbeid <ubead95@gmail.com>
2019-07-09 15:00:49 +0300

    Add changes on tests for addToList function Relates #4

--- test.js
+++ test.js
@@ -1,32 +1,34 @@
 var test = require('tape');
 var logic = require('./logic');
 var state = [
-  { id: -3, description: 'first todo' },
-  { id: -2, description: 'second todo' },
-  { id: -1, description: 'third todo' },
+  { id: -3, description: 'first todo', done: false },
+  { id: -2, description: 'second todo', done: false },
+  { id: -1, description: 'third todo', done: false },
 ];
 //console.log(logic);
 test('test add', function(t) {
   const actual1 = logic.addTodo(state,'mai');
-  const expected1 = [{ id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
-    { id: 1, description: 'mai' }
+  const expected1 = [
+    { id: -3, description: 'first todo', done: false },
+    { id: -2, description: 'second todo', done: false },
+    { id: -1, description: 'third todo', done: false },
+    { id: 1, description: 'mai', done: false }
   ];
   t.deepEqual(actual1, expected1 , 'Should return the new to do');
-
+  
   const actual2 = logic.addTodo(state,'nn');
-  const expected2 = [{ id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
-    { id: 2, description: 'nn' }
+  const expected2 = [
+    { id: -3, description: 'first todo', done: false },
+    { id: -2, description: 'second todo', done: false },
+    { id: -1, description: 'third todo', done: false },
+    { id: 2, description: 'nn', done: false }
   ];
   t.deepEqual(actual2, expected2 , 'Should return the new to do');
   
   t.deepEqual(state, [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
-  ], 'State should not be changed');
+    { id: -3, description: 'first todo', done: false },
+    { id: -2, description: 'second todo', done: false },
+    { id: -1, description: 'third todo', done: false },
+  ], 'State should not be changed');   
   t.end();
 });
